d328fc8129a66619134681725acc57824392b7ca
Edwin Shepherd <shardros@gmail.com>
2022-08-19 17:40:29 +0100

    add tests for creation and deletion

--- test/convenience.py
+++ test/convenience.py
@@ -1,30 +1,29 @@
 """A set of functions to make writing the tests a bit easier"""
+from pprint import pprint
 import time
-import pprint
 
 from fastapi.testclient import TestClient
 
 
 def wait_until(expr, interval=0.1, timeout=5):
     start = time.time()
-    while not expr:
+    while not expr():
         if time.time() - start > timeout:
             raise TimeoutError(
-                f"wait_until timeout occurred after {timeout}s: {lhs} != {rhs}")
+                f"wait_until timeout occurred after {timeout}s: {expr}")
         time.sleep(interval)
 
 
 def start_python(client: TestClient, python_file: str):
-    files = {"file": (python_file, open(python_file, "rb"))}
-    response = client.post("/upload", files=files)
-    pprint.pprint(response.content)
-    print(python_file)
+    files = {"uploaded_file": (python_file, open(python_file, "rb"))}
+    response = client.post("/upload/upload", files=files)
     assert response.status_code == 201
     time.sleep(1)  # TODO: https://github.com/systemetric/shepherd-2/issues/18
-    response = client.get("/start")
+    response = client.post("/run/start")
+    wait_until(lambda: client.get("run/state").json() == "Running", timeout=5, interval=0.01)
 
 
 def run_python(client: TestClient, python_file: str):
     start_python(client, python_file)
-    wait_until(lambda: client.get("/state").json() == "Stopped", timeout=2)
-    return client.get("/logs").json()
+    wait_until(lambda: client.get("run/state").json() == "Stopped", timeout=2)
+    return client.get("run/logs").json()
--- test/test_runner.py
+++ test/test_runner.py
@@ -11,21 +11,22 @@ client = TestClient(app)
 @pytest.mark.xfail(reason="Ready state not implemented, https://github.com/systemetric/shepherd-2/issues/18")
 def test_inital_state():
     """The server is ready to run code within timeout seconds of starting"""
-    wait_until(lambda: client.get("/state").json() == "Ready")
-    assert client.get("/state").status_code == 200
+    wait_until(lambda: client.get("/run/state").json() == "Ready")
+    assert client.get("/run/state").status_code == 200
 
 def test_start():
     """Code can be started"""
-    client.get("/start")
-    wait_until(lambda: client.get("/state").json() == "Running")
-    assert client.get("/state").status_code == 200
+    client.post("/run/start")
+    wait_until(lambda: client.get("/run/state").json() == "Running")
+    response = client.get("/run/state")
+    assert response.status_code == 200
 
 def test_stop():
     """Code can be stopped within TIMEOUT seconds of it commanding to be stopped"""
-    client.get("/start")
-    client.get("/stop")
-    wait_until(lambda: client.get("/state").json() == "Stopped")
-    assert client.get("/state").status_code == 200
+    client.post("/run/start")
+    client.post("/run/stop")
+    wait_until(lambda: client.get("/run/state").json() == "Stopped")
+    assert client.get("/run/state").status_code == 200
 
 def test_kill_user_code():
     """Usercode can be killed
@@ -33,9 +34,9 @@ def test_kill_user_code():
     code is stopped that the output does not continue to grow.
     """
     start_python(client, "test/stimulus/while_true.py")
-    client.get("/stop")
-    wait_until(lambda: client.get("/state").json() == "Stopped")
-    log1 = client.get("/logs").json()
-    time.sleep(0.1)
-    log2 = client.get("/logs").json()
+    client.post("/run/stop")
+    wait_until(lambda: (client.get("/run/state").json() == "Stopped"))
+    log1 = client.get("/run/logs").json()
+    time.sleep(1)
+    log2 = client.get("/run/logs").json()
     assert(log1 == log2)
